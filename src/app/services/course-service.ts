bfee08701d97db36b7e0ba77947996a69d8dcc35
m.couderc <m.couderc@komet-is.fr>
2026-01-28 14:48:07 +0100

    feat: implement CourseService with HTTP client

--- src/app/services/course-service.ts
+++ src/app/services/course-service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../models/course.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
-  
+  private dataUrl = 'assets/data.json';
+
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.dataUrl);
+  }
 }
