161fe758f822c3ce6cb8280b201478b3d4850b42
m.couderc <m.couderc@komet-is.fr>
2026-01-28 14:51:22 +0100

    feat: implement course component

--- src/app/course/course.ts
+++ src/app/course/course.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Course as CourseModel } from '../models/course.model';
 
 @Component({
   selector: 'app-course',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './course.html',
   styleUrl: './course.css',
 })
 export class Course {
-
+  @Input() course!: CourseModel;
 }
