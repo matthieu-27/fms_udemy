ac2864fbd47f04f17dadcb954c512ffab4849617
m.couderc <m.couderc@komet-is.fr>
2026-01-28 14:26:48 +0100

    feat: add course model

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -1,2 +1,13 @@
 export class Course {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+
+  constructor(id: number, name: string, description: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
 }
