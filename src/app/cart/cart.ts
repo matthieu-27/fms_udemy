9de775ffe64bc5ced6cf74ecab6206c889a657b7
matthieu-27 <matthieu27@pm.me>
2026-01-29 09:40:36 +0100

    feat: implement cartComponent

--- src/app/cart/cart.ts
+++ src/app/cart/cart.ts
@@ -1,11 +1,35 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartItem } from '../models/cart-item.model';
+import { CartService } from '../services/cart-service';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CurrencyPipe],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class Cart {
+  cartItems: CartItem[] = [];
+  total: number = 0;
 
+  constructor(private cartService: CartService) {
+    this.cartService.cartItems$.subscribe((items) => {
+      this.cartItems = items;
+      this.calculateTotal();
+    });
+  }
+
+  calculateTotal() {
+    this.total = this.cartItems.reduce((sum, item) => sum + item.course.price * item.quantity, 0);
+  }
+
+  removeItem(item: CartItem) {
+    this.cartService.removeFromCart(item);
+  }
+
+  clearCart() {
+    this.cartService.clearCart();
+  }
 }
