170106725877f57dc7374215c4ef418546b359b3
matthieu-27 <matthieu27@pm.me>
2026-01-29 14:41:29 +0100

    feat: update course model

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -3,11 +3,13 @@ export class Course {
   name: string;
   description: string;
   price: number;
+  category: string;
 
-  constructor(id: number, name: string, description: string, price: number) {
+  constructor(id: number, name: string, description: string, price: number, category: string = 'Autre') {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
+    this.category = category;
   }
 }
