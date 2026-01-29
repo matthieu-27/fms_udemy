f73ba66a80ae0ddbd5b949307deb89408f8323f9
matthieu-27 <matthieu27@pm.me>
2026-01-29 10:54:59 +0100

    feat: implement checkout form

--- src/app/checkout/checkout.ts
+++ src/app/checkout/checkout.ts
@@ -1,11 +1,50 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CartService } from '../services/cart-service';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [ReactiveFormsModule, CurrencyPipe],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class Checkout {
+  checkoutForm: FormGroup;
+  cartItems: any[] = [];
+  total: number = 0;
 
+  constructor(
+    private fb: FormBuilder,
+    private cartService: CartService,
+  ) {
+    this.checkoutForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+    });
+
+    // Récupérer les données du panier
+    this.cartService.cartItems$.subscribe((items) => {
+      this.cartItems = items;
+      this.total = this.cartService.getTotal();
+    });
+  }
+
+  onSubmit() {
+    if (this.checkoutForm.valid) {
+      const orderData = {
+        customer: this.checkoutForm.value,
+        items: this.cartItems,
+        total: this.total,
+        date: new Date(),
+      };
+
+      console.log('Commande validée:', orderData);
+      alert('Commande validée avec succès!');
+    } else {
+      alert('Veuillez remplir tous les champs correctement.');
+    }
+  }
 }
