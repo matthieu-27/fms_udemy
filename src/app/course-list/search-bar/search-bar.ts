38509350e046b60d33fc4b940b8877ee30cad869
matthieu-27 <matthieu27@pm.me>
2026-01-29 14:19:51 +0100

    feat: implement two way binding

--- src/app/course-list/search-bar/search-bar.ts
+++ src/app/course-list/search-bar/search-bar.ts
@@ -1,5 +1,7 @@
-import { Component, model, output } from '@angular/core';
+import { Component, Input, model, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Course } from '../../models/course.model';
+import { CartService } from '../../services/cart-service';
 
 @Component({
   selector: 'app-search-bar',
@@ -11,6 +13,10 @@ import { FormsModule } from '@angular/forms';
 export class SearchBar {
   searchStr = model('');
   searchStrChange = output<string>();
+  
+  @Input() filteredCourses: Course[] = [];
+
+  constructor(private cartService: CartService) {}
 
   onSearchChange(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -22,5 +28,19 @@ export class SearchBar {
   clearSearch() {
     this.searchStr.set('');
     this.searchStrChange.emit('');
+  }
+
+  // Méthode pour ajouter toutes les formations filtrées au panier
+  addAllToCart() {
+    if (this.filteredCourses.length === 0) {
+      alert('Aucune formation trouvée à ajouter au panier.');
+      return;
+    }
+
+    this.filteredCourses.forEach(course => {
+      this.cartService.addToCart(course);
+    });
+
+    alert(`${this.filteredCourses.length} formation(s) ajoutée(s) au panier !`);
   }
 }
