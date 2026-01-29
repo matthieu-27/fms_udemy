2e1eb08d7312a35195e108e0a270e3145a1e4ea4
matthieu-27 <matthieu27@pm.me>
2026-01-29 08:48:06 +0100

    fix: added default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { CourseList } from './course-list/course-list';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/courses',
+    pathMatch: 'full'
+  },
+  {
     path: 'courses',
     component: CourseList,
   },
