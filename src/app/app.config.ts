e643c0dfb2d537b3d0899127e4f71f356f7869fc
matthieu-27 <matthieu27@pm.me>
2026-02-02 13:10:28 +0100

    feat: configure httpClient to handle json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withJsonpSupport,
+  withNoXsrfProtection,
+} from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -9,7 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withNoXsrfProtection(), withFetch(), withJsonpSupport()),
     provideZard(),
   ],
 };
