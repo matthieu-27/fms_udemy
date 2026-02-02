a00347b372c612361c1a01fff623be204c4b57c1
matthieu-27 <matthieu27@pm.me>
2026-02-02 09:23:08 +0100

    feat: add zard ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
+import { provideZard } from '@/shared/core/provider/providezard';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+    provideZard(),
+  ],
 };
