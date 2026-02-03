a93ecda3af133ea5b1da9d39e6be1cdd601c295d
matthieu-27 <matthieu27@pm.me>
2026-02-03 13:36:31 +0100

    feat: add admin page userService call

--- src/app/admin-page/admin-page.ts
+++ src/app/admin-page/admin-page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, model, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ZardButtonComponent } from '@/shared/components/button';
@@ -6,8 +6,8 @@ import { ZardCardComponent } from '@/shared/components/card';
 import { ZardFormImports } from '@/shared/components/form/form.imports';
 import { ZardInputDirective } from '@/shared/components/input';
 
-import { Roles, User as UserModel } from '@/models/user.model';
-import * as bcrypt from 'bcryptjs';
+import { UserService } from '@/services/user-service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-admin-page',
@@ -26,44 +26,41 @@ import * as bcrypt from 'bcryptjs';
   styleUrl: './admin-page.css',
 })
 export class AdminPage {
-  private users: UserModel[] = [
-    { email: 'elbab@gmail.com', password: '1234', roles: Roles.Admin },
-    { email: 'hugo@gmail.com', password: '1234', roles: Roles.User },
-    { email: 'test@gmail.com', password: '1234', roles: Roles.Customer },
-  ];
-  user = model<UserModel>();
+  submitted = false;
 
   profileForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
+  constructor(private userService: UserService) {}
+
   onSubmit() {
-    console.log(this.profileForm.value);
-    console.log(this.checkUser(this.profileForm.value.email!, this.profileForm.value.password!));
-    const salt = bcrypt.genSaltSync(10);
-    const hashPass = bcrypt.hashSync(this.profileForm.value.password!, salt);
-    console.log(hashPass);
+    if (this.profileForm.valid) {
+      this.submitted = true;
+      if (
+        !this.userService.checkUser(this.profileForm.value.email!, this.profileForm.value.password!)
+      ) {
+        this.submitted = false;
+        toast.error("Erreur d'identification", {
+          description: "L'email ou le mot de passe sont incorrect",
+        });
+      }
+    }
   }
 
   /**
-   * Checks if user is in `users` table
-   * @param email
-   * @param password
+   * emailControl getter
    */
-  checkUser(email: string, password: string): boolean {
-    for (const user of this.users) {
-      if (user.email == email && user.password == password) {
-        this.user.set(new UserModel(email, password, user.roles));
-        return true;
-      } else {
-        this.user.set(undefined);
-      }
-    }
-    return false;
-  }
-
   get emailControl() {
     return this.profileForm.get('email')!;
+  }
+
+  /**
+   * Reset Form button
+   */
+  resetForm() {
+    this.profileForm.reset();
+    this.submitted = false;
   }
 }
