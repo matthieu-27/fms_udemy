3abb775591041b89aa37269ec885d255c41a130d
matthieu-27 <matthieu27@pm.me>
2026-02-03 10:54:11 +0100

    feat: implement userService

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,17 +1,11 @@
 export class User {
   email: string;
   password: string;
-  roles: Roles;
+  roles: string[];
 
-  constructor(email: string, password: string, roles: Roles) {
+  constructor(email: string, password: string, roles: string[]) {
     this.email = email;
     this.password = password;
     this.roles = roles;
   }
-}
-
-export enum Roles {
-  Admin = 'ADMIN',
-  User = 'USER',
-  Customer = 'CUSTOMER',
 }
--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,8 +1,38 @@
+import { User as UserModel } from '@/models/user.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as bcrypt from 'bcryptjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  private dataUrl = 'http://localhost:3000/users';
+  private salt = bcrypt.genSaltSync(10);
+  private users: UserModel[] = [];
+
+  constructor(private http: HttpClient) {
+    this.getUsers().subscribe((users) => {
+      this.users = users;
+    });
+  }
+
+  getUsers(): Observable<UserModel[]> {
+    return this.http.get<UserModel[]>(this.dataUrl);
+  }
+
+  /**
+   * Checks if user is in `users` table
+   * @param email
+   * @param password
+   */
+  checkUser(email: string, password: string): boolean {
+    for (const user of this.users) {
+      if (user.email == email && user.password == password) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
