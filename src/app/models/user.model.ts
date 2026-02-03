e9aca3e1178679b01378a2b91cbb3a313cd90257
matthieu-27 <matthieu27@pm.me>
2026-02-03 08:35:21 +0100

    feat: add user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+  email: string;
+  password: string;
+  roles: string[];
+
+  constructor(email: string, password: string, roles: string[]) {
+    this.email = email;
+    this.password = password;
+    this.roles = roles;
+  }
+}
