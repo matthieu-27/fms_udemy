e1d66d584d9126d658189ef3d027a1c7145144a3
matthieu-27 <matthieu27@pm.me>
2026-02-03 09:20:50 +0100

    feat: add user roles enum

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,11 +1,17 @@
 export class User {
   email: string;
   password: string;
-  roles: string[];
+  roles: Roles;
 
-  constructor(email: string, password: string, roles: string[]) {
+  constructor(email: string, password: string, roles: Roles) {
     this.email = email;
     this.password = password;
     this.roles = roles;
   }
+}
+
+export enum Roles {
+  Admin = 'ADMIN',
+  User = 'USER',
+  Customer = 'CUSTOMER',
 }
