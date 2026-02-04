17aa71ce865bcf119620493c6ca341ef10039e9b
m.couderc <m.couderc@komet-is.fr>
2026-02-04 14:31:22 +0100

    feat: implement dark mode

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,34 @@
+import { UserService } from '@/services/user-service';
+import { ZardIconComponent } from '@/shared/components/icon';
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, Renderer2, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, NgOptimizedImage],
+  imports: [RouterLink, NgOptimizedImage, ZardIconComponent],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  theme = signal<'light' | 'dark'>('light');
+  role = signal('CUSTOMER');
+  isLoggedIn = signal(false);
+
+  themeClass = computed(() => `theme-${this.theme()}`);
+
+  constructor(
+    private userService: UserService,
+    private renderer: Renderer2,
+  ) {}
+
+  toggleTheme() {
+    if (this.theme() == 'light') {
+      this.renderer.addClass(document.body.parentElement, 'dark');
+      this.theme.set('dark');
+    } else {
+      this.renderer.removeClass(document.body.parentElement, 'dark');
+      this.theme.set('light');
+    }
+  }
+}
