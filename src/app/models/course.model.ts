742ac4b57827f0a93fa7758af8c01d63394d4473
matthieu-27 <matthieu27@pm.me>
2026-02-05 14:48:45 +0100

    feat: add interface iCourseForm

--- src/app/models/course.model.ts
+++ src/app/models/course.model.ts
@@ -5,11 +5,25 @@ export class Course {
   price: number;
   category: string;
 
-  constructor(id: number, name: string, description: string, price: number, category: string = 'Autre') {
+  constructor(
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    category: string = 'Autre',
+  ) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.price = price;
     this.category = category;
   }
+}
+
+export interface iCourseForm {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
 }
