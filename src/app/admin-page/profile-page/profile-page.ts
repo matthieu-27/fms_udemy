48b40b56caea79c196956ec7aa447536d507d3d6
matthieu-27 <matthieu27@pm.me>
2026-02-09 09:50:25 +0100

    feat: add edit route component

--- src/app/admin-page/profile-page/profile-page.ts
+++ src/app/admin-page/profile-page/profile-page.ts
@@ -1,5 +1,8 @@
+import { Course as CourseModel } from '@/models/course.model';
+import { CourseService } from '@/services/course-service';
 import { UserService } from '@/services/user-service';
-import { Component, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile-page',
@@ -8,8 +11,16 @@ import { Component, signal } from '@angular/core';
   styleUrl: './profile-page.css',
 })
 export class ProfilePage {
+  private activatedRoute = inject(ActivatedRoute);
   isAdmin = signal(false);
-  constructor(private userService: UserService) {
+  course = input.required<CourseModel>();
+
+  constructor(
+    private userService: UserService,
+    private courseService: CourseService,
+  ) {
     this.isAdmin.set(this.userService.isBigBoss());
+
+    console.log(this.activatedRoute);
   }
 }
