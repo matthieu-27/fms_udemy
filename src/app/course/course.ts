ffe53a96a49a62bad3fa41f134cac878cc60250e
matthieu-27 <matthieu27@pm.me>
2026-02-09 14:02:58 +0100

    feat: add delete course handler in course component

--- src/app/course/course.ts
+++ src/app/course/course.ts
@@ -1,7 +1,9 @@
+import { CourseService } from '@/services/course-service';
 import { UserService } from '@/services/user-service';
 import { ZardAlertDialogService } from '@/shared/components/alert-dialog';
 import { ZardButtonComponent } from '@/shared/components/button';
 import { ZardCardComponent } from '@/shared/components/card';
+import { ZardDividerComponent } from '@/shared/components/divider';
 import { ZardIconComponent } from '@/shared/components/icon';
 import { ZardMenuImports } from '@/shared/components/menu';
 import { CurrencyPipe } from '@angular/common';
@@ -19,6 +21,7 @@ import { CartService } from '../services/cart-service';
     ZardIconComponent,
     ZardCardComponent,
     ZardMenuImports,
+    ZardDividerComponent,
   ],
   templateUrl: './course.html',
   styleUrl: './course.css',
@@ -31,6 +34,7 @@ export class Course {
   constructor(
     private cartService: CartService,
     private userService: UserService,
+    private courseService: CourseService,
     private router: Router,
   ) {
     this.userService.isBigBoss() ? this.isBigBoss.set(true) : null;
@@ -48,17 +52,39 @@ export class Course {
     console.log('Navigate to:', item);
   }
 
+  create() {
+    this.router.navigateByUrl('adminpage');
+  }
+
   edit(id: number) {
     this.router.navigateByUrl('adminpage/' + id);
   }
 
-  showDialog() {
+  async showDeleteDialog() {
     this.alertDialogService.confirm({
       zTitle: 'Are you absolutely sure?',
       zDescription:
         'This action cannot be undone. This will permanently delete the course and remove the data from our server.',
       zOkText: 'Continue',
       zCancelText: 'Cancel',
+      zOnOk: () => {
+        this.handleDeleteById();
+      },
     });
+  }
+
+  async handleDeleteById() {
+    this.deleteCourseById();
+  }
+
+  deleteCourseById() {
+    if (this.courseService.deleteCourseById(this.course().id)) {
+      toast.success('Cours Supprimé avec succès', {
+        description: `${this.course().name}: ${this.course().description} n'existe plus sur nos serveurs`,
+      });
+      this.router.navigateByUrl('courses');
+    } else {
+      toast.error('Un problème est arrivé');
+    }
   }
 }
